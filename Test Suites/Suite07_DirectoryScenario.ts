afb39bb9b52fa83da52518b58d473fa4c02e0e94
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 15:18:50 +0530

    Commit Directory

---
+++ Test Suites/Suite07_DirectoryScenario.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite07_DirectoryScenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>499f4492-3a6d-4436-973c-f14b5294bdf9</testSuiteGuid>
+   <testCaseLink>
+      <guid>eaf92045-330a-452a-995f-2ccf76fc50a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC07_DirectoryScenario</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
