bd18ca703bb0d5193dae547bfd26238745a5a933
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 15:29:48 +0530

    Commit 11

---
+++ Test Suites/Suite_ClaimScenario.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_ClaimScenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4c08d395-7703-4cd2-9f3c-013b399f2eba</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c1a9a87-8083-422c-ae39-5780acbd48af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC08_ClaimScenario</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
