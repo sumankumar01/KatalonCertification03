b0880bee7ef89de8df0278e3ad458dec5c8c3f56
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 15:06:42 +0530

    Commit Recuirtment

---
+++ Test Suites/Suite05_Rectuirment.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite05_Rectuirment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0802ea67-9151-4940-90da-8128656e1591</testSuiteGuid>
+   <testCaseLink>
+      <guid>93883338-5625-4fe7-9a72-fc5c25c3f965</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC05_Rectuirment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
