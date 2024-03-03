087866f83a679cb76351fdc7ef6b57fc13a6f619
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 15:37:59 +0530

    Buzz

---
+++ Test Suites/Suite_BuzzScenario.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_BuzzScenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>89e54f96-c43c-4e29-8589-362a2bcdd157</testSuiteGuid>
+</TestSuiteEntity>
