28c7955592f09d4a2940658b7333c21c71a79d7d
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 16:02:59 +0530

    sad

---
+++ Test Suites/suite13_LeaveList.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suite13_LeaveList</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ce4033d-b39a-40f2-ab01-d526981f7118</testSuiteGuid>
+   <testCaseLink>
+      <guid>2d83be4d-1b91-494c-aee9-921968650a56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC13_LeaveList</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
