10bf27bce869ce11ce77cce3883aca93895bf331
YH743DF <YH743DF@IN3106855W3.ey.net>
2024-03-03 15:59:03 +0530

    sadas

---
+++ Test Suites/suite12_ConfigureLeavePeriods.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suite12_ConfigureLeavePeriods</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a36c2578-7e2c-4f24-bf63-7cd21ec775b6</testSuiteGuid>
+</TestSuiteEntity>
